17fdf7f3aa5d2bb7f62c1276b97c6a0dc3399945
Pontusun <xsunguanx@gmail.com>
2015-10-18 13:40:03 -0400

    add face recognation code

---
+++ face/CMakeFiles/facerec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facerec.dir/facerec.cpp.o"
+  "facerec.pdb"
+  "facerec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facerec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
