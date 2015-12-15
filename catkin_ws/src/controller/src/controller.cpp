675e3e0ec989b5a0781f435ac42632cc8ca13a33
andylee024 <andylee024@gmail.com>
2015-12-15 00:25:37 -0500

    cleaned up controller

--- catkin_ws/src/controller/src/controller.cpp
+++ catkin_ws/src/controller/src/controller.cpp
@@ -4,10 +4,14 @@
 #include <hector_nav_msgs/GetRobotTrajectory.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+
+// Messages 
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/String.h>
+#include <std_msgs/Float32MultiArray.h>
+
 // #include "face_recognition/Loc.h"
 
 // Libraries
@@ -27,27 +31,26 @@ public:
     // constructor
     Controller(ros::NodeHandle& nh){
 
-        //subscribers and publishers
+        //publishers
         commandPub = nh.advertise < geometry_msgs::Twist > ("cmd_vel_mux/input/navi", 1);
-        objectSub = nh.subscribe("caffe_ret", 1, &Controller::detectObject, this);
+        androidPub = nh.advertise < std_msgs::String>("android_fb", 1000);
+
+        //subscribers
+        //faceSub = nh.subscribe("face_ret")
         amclSub = nh.subscribe("amcl_pose",1, &Controller::amclCallback,this);
+        androidSub = nh.subscribe("android_ui", &Controller::androidCallback, this);
 
         //clients
         exploration_plan_service_client_ = nh.serviceClient<hector_nav_msgs::GetRobotTrajectory>("get_exploration_path");
     	path_follower_.initialize(&tfl_);
         mb_ac = new MoveBaseClient("move_base", true); 
 		while(!mb_ac->waitForServer(ros::Duration(5.0))) { 
-  			ROS_INFO("Waiting for the move_base action server to come up"); 
+  			ROS_INFO("Waiting for the move_base action server to come up");
   		}
-	
-    	//configurations
-    	objectTargets.push_back("keyboard");
-    	objectTargets.push_back("ball");
-    	objectTargets.push_back("chair");
 
-        //states 
-    	detect_face = false;
-    	detect_obj = false;
+  		// controller members
+  		FSM fsm = GOTO;
+	 
     }
     
     // destructor
@@ -56,6 +59,7 @@ public:
         delete mb_ac;
     }
     
+
 
 
 /******************************
@@ -88,7 +92,6 @@ public:
         this->mb_ac->cancelGoal(); 
     }
     
-    // send a velocity command
     void move(double linear, double angular){
         geometry_msgs::Twist msg; // The default constructor will set all commands to 0
         msg.linear.x = linear;
@@ -96,10 +99,8 @@ public:
         commandPub.publish(msg);
     }
 
-    // procedure for looking for object 
     void lookForOjbect(){    
         ROS_INFO("searching for object ...");
-        
         for (int i=0; i<20; i++){
             this->move(0.0,0.75);
             ROS_INFO("turning...");
@@ -111,28 +112,26 @@ public:
     		    }
 		    }	
         }
-
 	    ROS_INFO("finished looking...");
     }
 
-    // goal based navigation
     bool goal(std::vector<double> v){
         double x = v[0];
         double y = v[1];
         double yaw = v[2];
 
     	move_base_msgs::MoveBaseGoal goal;
-    	goal.target_pose.header.frame_id = "/map"; //"base_link";
+    	goal.target_pose.header.frame_id = "/map"; 
     	goal.target_pose.header.stamp = ros::Time::now();
     	goal.target_pose.pose.position.x = x;
     	goal.target_pose.pose.position.y = y;
     	goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
-  		//ROS_INFO("Sending a new goal to move_base x %lf y %lf yaw %lf", x, y, yaw);
   		this->mb_ac->sendGoal(goal);
+  		//ROS_INFO("Sending a new goal to move_base x %lf y %lf yaw %lf", x, y, yaw);
 
         if (this->mb_ac->waitForResult(ros::Duration(10.0)) ){
             return true;
-        };  
+        }
         this->mb_ac->cancelGoal();
         return false; 
     }
@@ -144,111 +143,109 @@ public:
 
 
 /*
-* Navigation Callbacks
+* Callbacks
 */
+
     void amclCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
-        ROS_INFO("reaching amcl callback");
         float x = msg->pose.pose.orientation.x;
         float y = msg->pose.pose.orientation.y;
         float z = msg->pose.pose.orientation.z;
         float w = msg->pose.pose.orientation.w;
-        ROS_INFO("current_pose |  x: %lf, y: %lf, z: %lf, w: %lf", x, y, z, w);
+        ROS_INFO("AMCL current_pose |  x: %lf, y: %lf, z: %lf, w: %lf", x, y, z, w);
     }
-
-/*
-* Vision Callbacks 
-*/
-
-    // TODO: Additional Vision subscribers  
     
-    void detectObject(const std_msgs::String::ConstPtr& msg){
-	   ROS_INFO("Vision callback");
-	   ROS_INFO("Caffe says: [%s]", msg->data.c_str());
+    void caffeCallback(const std_msgs::String::ConstPtr& msg){
+	    ROS_INFO("Caffe says: [%s]", msg->data.c_str());
     	
         // extract information from message
     	std::string line;
     	std::string object;
-
     	line = msg->data.c_str();	
     	object = line.substr(line.find_first_of(" \t")+1);
     	
-        //ROS_INFO_STREAM("Current CAFFE object: " << object);
     	for (int i =0; i<this->objectTargets.size(); i++) {
-    		if (object.find(this->objectTargets[i])!= std::string::npos){
+    		if (object.find(this->objectTargets[i])!= std::string::npos){ // parse caffe message for item
     			ROS_INFO_STREAM("FOUND: " << object);
     			this->detect_obj = true;
     		}
 	   }    
     }
 
+    void objectCallback(const std_msgs::Float32MultiArray::ConstPtr& msg){
+        ROS_INFO("FILL IN OBJECT CALLBACK");
+    }
 
+    void androidCallback(const std_msgs::Float32MultiArray::ConstPtr& msg){
+        ROS_INFO("FILL IN ANDROID CALLBACK");
+    }
 
-// Finite State Machine Loop
-// spin() should be overloaded for each individual task controller
-    
-    void spin(){
-        // initialize values 
-        double explore_x = 2.0;
-        double explore_y = -11.9994510184;
-        double explore_w = 0.995189973514;
-        double explore_z = -0.0979638536269;
-        double explore_yaw = get_yaw(explore_x,explore_y,explore_w,explore_z);
+    void faceCallback(const std_msgs::Float32MultiArray::ConstPtr& msg){
+    	ROS_INFO("FILL IN FACE CALLBACK");
+    }
+
+/******************
+* Spin() for Tasks
+******************/
+
+    void spin_findObject(){
         
-        std::vector<double> homeBase;
+        // setup task specific variables 
+        caffeSub = nh.subscribe("caffe_ret", 1, &Controller::caffeCallback, this);
+        objectSub = nh.subsribe("objects", 1, &Controller::objectCallback, this);
+    	std::vector<double> navGoal; // [x,y,yaw]
+		std::vector<double> homeBase;
+		std::vector<std::string> objectTargets;
+		bool detect_obj = false;
+  
+		// initialize variables 
         homeBase.push_back(2.40424320025);
         homeBase.push_back(1.33934123128);
         homeBase.push_back(3.3); 
+        navGoal.push_back(2.0);
+        navGoal.push_back(-11.0);
+        navGoal.push_back(-1.4);
 
-        std::vector<double> exploreArea;
-        exploreArea.push_back(2.0);
-        exploreArea.push_back(-11.0);
-        exploreArea.push_back(-1.4);
         
-        this->state = 1;
-        this->goto_Goal = exploreArea;
-
+        // spin loop 
+        this->fsm = GOTO;
         ros::Rate rate(50);
         while (ros::ok()) {
+        	switch(fsm){
 
-            // go to exploration area 
-            if (state == 1){
-                bool result = goal(goto_Goal); 
-                if (result == true && this->goto_Goal == homeBase){
-                    ROS_INFO("Wiggles has returned to homebase");
-                    ROS_INFO("exiting");
-                    break;
-                }
+        		case GOTO : 
+        			bool result = goal(navGoal);
+        			if (result == true && this->navGoal == homeBase){
+	                    ROS_INFO("Wiggles has returned to homebase");
+	                    ROS_INFO("exiting");
+	                    return;
+                	}	
 
-                if( result ){ // reached goal 
-                    this->state = 2;
-                }
-            }
-            
-            // explore area for object
-            if (state == 2){
-                if( detect_obj == false){
-                    this->navigationExplore();
-                    this->lookForOjbect();
-                }
-                if (detect_obj == true){ // found object
-                    this->state = 3;
-                }
-            }
+                	if (result){
+                		this->fsm = EXPLORE;
+                	}
+        			break;
 
-            // wait and puase for 10 seconds
-            if (state == 3){ 
-                move(0.0,0.0);
-                for (int i = 0; i<10; i++){
-                    ros::Duration(1.0).sleep();
-                    ROS_INFO("object found, waiting ...");
-                }
-                this->goto_Goal = homeBase;
-                this->state = 1;
-            }
+        		case EXPLORE : 
+        			if( detect_obj == true){
+        				this->FSM = WAIT;
+        				break;
+                	}
+                	this->navigationExplore();
+	                this->lookForOjbect();
+        			break;
 
-    	    ros::spinOnce();
-            rate.sleep();
+        		case WAIT :
+        			move(0.0,0.0);
+	                ROS_INFO("object found, waiting ...");
+	                ros::Duration(10.0).sleep();
+                	navGoal = homeBase;
+                	this->fsm = GOTO;
+        			break;  
+        	}
         }
+
+    	ros::spinOnce();
+        rate.sleep();
     }
 
 protected:
@@ -267,13 +264,8 @@ protected:
 	ros::Timer cmd_vel_generator_timer_;
 	MoveBaseClient *mb_ac; 
 
-	// flags and states	
-	//enum FSM state;
-	std::vector<std::string> objectTargets;
-	bool detect_face;
-	bool detect_obj;
-    int state;
-    std::vector<double> goto_Goal;
+	// controller members
+	enum FSM {GOTO, EXPLORE, WAIT, FOLLOW};
 };
 
 
@@ -298,3 +290,14 @@ int main(int argc, char **argv) {
 */
 
 
+/*
+
+    	//configurations
+    	objectTargets.push_back("keyboard");
+    	objectTargets.push_back("ball");
+    	objectTargets.push_back("chair");
+
+        //states 
+    	detect_face = false;
+    	detect_obj = false;
+*/
